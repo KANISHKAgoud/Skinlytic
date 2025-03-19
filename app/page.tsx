65cd01e68fee458206df0db33cafc6dd465d41d7
kuki <goudkanishka@gmail.com>
2025-03-19 23:44:11 +0530

    public folder

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ export default function Home() {
     <div
       className="h-screen w-full bg-center bg-cover"
       style={{ backgroundImage: "url('/images/background-image.jpg')" }}>
-      <h1 className="text-white text-4xl font-bold flex justify-center items-center h-full">
+      {/* <h1 className="text-white text-4xl font-bold flex justify-center items-center h-full">
         Welcome to Skinlytic
-      </h1>
+      </h1> */}
+
+      <div className="h-screen bg-home flex items-center justify-center text-white">
+        <div className="text-center bg-opacity-50 p-10 rounded-lg">
+          <h1 className="text-5xl font-bold">Welcome to Skinlytic</h1>
+          <p className="mt-4 text-lg">Your AI-powered skincare assistant</p>
+        </div>
+      </div>
     </div>
   );
 }
