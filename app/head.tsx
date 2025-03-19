c25cb54209b513103332760ebd7a2eaabd7ddb92
kuki <goudkanishka@gmail.com>
2025-03-20 00:16:51 +0530

    layout

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>My Next.js App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+      </>
+    );
+  }
+  
