40c75e88ecc03f743da1c2f4a322b0b3e450e0b5
Kuki <goudkanishka@gmail.com>
2025-03-25 10:45:11 +0530

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
             />
           </svg>
-          <div className="text-white ">
+          <div className="text-white flex flex-row">
 
           <Link href="/" onClick={() => setShowMenu(false)}>
           <li> Home</li>
